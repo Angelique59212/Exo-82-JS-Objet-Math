0294790daa271932fbb98190389d176dc3219698
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-25 15:16:40 +0100

    objet math

---
+++ exo.js
@@ -0,0 +1,79 @@
+//1
+
+let value1 = 34.589456;
+value1 = Math.floor(value1);
+console.log(value1);
+
+//2
+
+let value2 = 68.412784;
+value2 = Math.floor(value2);
+console.log(value2);
+
+//3
+
+let value3 = 39.456;
+value3 = Math.ceil(value3);
+console.log(value3);
+
+//4
+
+let value4 = 27.34876541;
+value4 = Math.ceil(value4);
+console.log(value4);
+
+//5
+
+let value5 = 5.68;
+value5 = Math.round(value5);
+console.log(value5);
+
+//6
+
+let value6 = 5.41;
+value6 = Math.round(value6);
+console.log(value6);
+
+//7
+
+let value7 = 6.12457;
+value7 = Math.trunc(value7);
+console.log(value7);
+
+//8
+
+let value8 = 9.568124;
+value8 = Math.trunc(value8);
+
+//9
+
+let a = 5, b=1, c=12, d=-1, e=45;
+let minimum = Math.min(a, b, c, d, e);
+console.log(minimum);
+
+//10
+
+let f= 9, g=12, h=16, i=44, j=3;
+let minimum1 = Math.min(f, g, h, i, j);
+console.log(minimum1);
+
+//11
+
+let k= 5, l=1, m= 12, n=-1, o=45;
+let maximum = Math.max(k, l, m, n, o);
+console.log(maximum);
+
+//12
+
+let p= 9, q= 12, r=16, s= 44, t=3;
+let maximum1= Math.max(p, q, r, s, t);
+console.log(maximum1);
+
+//13
+
+let absolute = Math.abs(-6);
+console.log(absolute);
+
+//14
+let absolute1 = Math.abs(-56);
+console.log(absolute1);
